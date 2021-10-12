61ea4167d849546f79b549e2ad5fe869c09ee188
Christoffer A Traeen <christoffer.andersen@live.com>
2021-10-12 21:51:22 +0200

    Create submit view

--- frontend/src/views/SubmitView.tsx
+++ frontend/src/views/SubmitView.tsx
@@ -1,8 +1,26 @@
+import { SendRounded } from '@material-ui/icons'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import ContentBox from '../components/ContentBox'
 
 /**
  * This is the view for submitting the secret data.
  */
 export default function SubmitView() {
-  return <div></div>
+  const { handleSubmit } = useForm()
+  return (
+    <ContentBox>
+      <div className='flex flex-col min-w-full'>
+        <h2 className='mb-5 uppercase'>Share secret</h2>
+        <textarea
+          placeholder='Paste your secret here...'
+          className='min-w-full rounded-md mb-5 h-28 bg-chambray-900 text-chambray-200 p-3 text-xs bg-opacity-30 focus:outline-none shadow-lg placeholder-chambray-600'
+        ></textarea>
+        <button className='flex justify-center items-center bg-chambray-400 px-6 py-2 rounded-md text-white uppercase hover:bg-chambray-300 transition-all flex-none text-xs'>
+          share
+          <SendRounded className='ml-3 text-xs' fontSize='small' />
+        </button>
+      </div>
+    </ContentBox>
+  )
 }
