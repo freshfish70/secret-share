d3aa946150cfa3a08751fb72c50d0a6c8bbd4563
Christoffer A Traeen <christoffer.andersen@live.com>
2021-10-12 21:51:01 +0200

    Create titled text container component

---
+++ frontend/src/components/TitledTextContainer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface TitledTextContainerProps {
+  title: string
+  text: string
+  subtext: string
+}
+export default function TitledTextContainer({ title, text, subtext }: TitledTextContainerProps) {
+  return (
+    <div className='my-2 p-3 rounded bg-chambray-700 hover:bg-chambray-700 transition-all cursor-pointer opacity-60 hover:opacity-100'>
+      <span className='block text-chambray-500 pt-2 hov'>{title}</span>
+      <div className='text-chambray-300 text-sm py-2 px-5 text-center'>{text}</div>
+      <div className='text-chambray-500 text-sm px-5 text-center'>{subtext}</div>
+    </div>
+  )
+}
