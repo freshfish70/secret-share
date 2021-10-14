ffa0db9c11b9e508a64f75c23a04b4625b23bb3d
Christoffer A Traeen <christoffer.andersen@live.com>
2021-10-14 22:18:04 +0200

    Refactor secret revealer

--- frontend/src/containers/SecretRevealer.tsx
+++ frontend/src/containers/SecretRevealer.tsx
@@ -1,4 +1,4 @@
-import { FileCopyOutlined, RemoveRedEye, Visibility, VisibilityOff } from '@material-ui/icons'
+import { FileCopyOutlined, Visibility, VisibilityOff } from '@material-ui/icons'
 import React, { useState } from 'react'
 import TextIconAction from '../components/TextIconAction'
 
@@ -18,40 +18,31 @@ export default function SecretRevealer({ title, content }: SecretRevealerProps) 
         </div>
         <div className='flex justify-center items-center'>
           <TextIconAction Icon={FileCopyOutlined} text={'Copy'} action={() => {}} />
-          <FileCopyOutlined fontSize='inherit' className='mr-1' />
-          <span className='text-xs mr-3 text-chambray-500 hover:text-chambray-400 transition-colors cursor-pointer'>
-            Copy
-          </span>
-          <span className='text-xs mr-1 text-chambray-500 hover:text-chambray-400 transition-colors cursor-pointer'>
-            {contentHidden ? (
-              <>
-                <Visibility fontSize='inherit' className='mr-2' />
-                Reveal
-              </>
-            ) : (
-              <>
-                <VisibilityOff fontSize='inherit' className='mr-2' />
-                Hide
-              </>
-            )}
-          </span>
+          <TextIconAction
+            Icon={contentHidden ? Visibility : VisibilityOff}
+            text={contentHidden ? 'show' : 'hide'}
+            action={() => {
+              setContentHidden(!contentHidden)
+            }}
+          />
         </div>
       </div>
-      <div className='min-w-full min-h-full relative'>
-        {contentHidden ? (
-          <div
-            onClick={() => setContentHidden(false)}
-            className='min-w-full min-h-full absolute flex flex-col justify-center items-center cursor-pointer hover:text-chambray-400 text-chambray-500 transition-colors'
-          >
-            <Visibility className='text-xs' fontSize='large' />
-            <span className='text-xs'>Click to reveal</span>
-          </div>
-        ) : (
-          <></>
-        )}
+      <div className='min-w-full min-h-full relative transition-all overflow-hidden'>
+        <div
+          onClick={() => setContentHidden(false)}
+          className={`${
+            contentHidden ? '' : 'opacity-0 -mt-40'
+          } min-w-full min-h-full absolute flex flex-col justify-center items-center cursor-pointer hover:text-chambray-400 text-chambray-500 transition-all`}
+        >
+          <Visibility className='text-xs' fontSize='large' />
+          <span className='text-xs'>Click to reveal</span>
+        </div>
         <textarea
           value={contentHidden ? '' : content}
-          className='min-w-full rounded-md h-28 bg-chambray-900 text-chambray-200 p-3 text-xs bg-opacity-30 focus:outline-none shadow-lg placeholder-chambray-600'
+          readOnly={true}
+          className={`${
+            contentHidden ? 'text-opacity-0' : 'text-opacity-100'
+          } transition-all min-w-full rounded-md h-28 bg-chambray-900 text-chambray-200 p-3 text-xs bg-opacity-30 focus:outline-none shadow-lg placeholder-chambray-600`}
         ></textarea>
       </div>
     </section>
