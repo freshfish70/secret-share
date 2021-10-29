2e7a190d96d5347dfc198d2280eee98a8da79af6
Christoffer A Traeen <christoffer.andersen@live.com>
2021-10-30 00:01:48 +0200

    Sample retrieve empty

--- frontend/src/views/RetrieveView.tsx
+++ frontend/src/views/RetrieveView.tsx
@@ -1,8 +1,10 @@
 import {
   EnhancedEncryptionRounded,
+  Error,
   EuroRounded,
   FileCopy,
   FileCopyOutlined,
+  Keyboard,
   PanoramaFishEye,
   RemoveRedEye,
   RemoveRedEyeOutlined
@@ -21,6 +23,14 @@ export default function RetrieveView() {
       content: 'eqjheqh khjqe khqwke hqwkehqkwheqwkjhckjhqkjhwerkhwervkjkwerv'
     },
     {
+      title: 'Password for gmail',
+      content: 'eqjheqh khjqe khqwke '
+    },
+    {
+      title: 'Password for gmail',
+      content: 'eqjheqh khjqe khqwke '
+    },
+    {
       title: 'Outlook content ',
       content: 'qkleq lkjeql kjeqlkjweljtkergk l;dbkl'
     }
@@ -29,8 +39,23 @@ export default function RetrieveView() {
     <ContentBox>
       <div className='min-w-full'>
         <h2>Retrieve secrets</h2>
+        <section className='my-3'>
+          <div className='min-w-full min-h-full relative transition-all overflow-hidden'>
+            <div
+              className={`animate-pulse min-w-full min-h-full absolute flex flex-col justify-center items-center text-yellow-500 transition-all`}
+            >
+              <Error className='' />
+              <span className='text-xs'>This bucket is empty</span>
+            </div>
+            <div
+              className={`${
+                true ? 'text-opacity-0' : 'text-opacity-100'
+              } transition-all min-w-full rounded-md h-28 bg-chambray-900 text-chambray-200 p-3 text-xs bg-opacity-30 focus:outline-none shadow-lg placeholder-chambray-600`}
+            ></div>
+          </div>
+        </section>
         {secrets.map((s, i) => {
-          return <SecretRevealer title={s.title} content={s.content} />
+          return <SecretRevealer key={i} title={s.title} content={s.content} />
         })}
       </div>
     </ContentBox>
