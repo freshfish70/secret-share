43753968bbc6ece2fe24e16523e1d8cd1b415c80
Christoffer A Traeen <christoffer.andersen@live.com>
2022-03-05 17:37:17 +0100

    Add query client provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,43 @@
 import React from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import IndexView from './views/IndexView'
 import RetrieveView from './views/RetrieveView'
 import SubmitView from './views/SubmitView'
 
+const queryClient = new QueryClient()
 function App() {
   return (
-    <div className='container mx-auto pb-10'>
-      <h1 className='pt-10 text-lg'>SECRET SHARE</h1>
-      <span className='text-xs uppercase text-chambray-600'>one way share using pub/priv keys</span>
-      <main className='flex justify-center my-10'>
-        <Router>
-          <Switch>
-            <Route exact path='/'>
-              <IndexView />
-            </Route>
-            <Route path='/retrieve/:shareId'>
-              <RetrieveView />
-            </Route>
-            <Route path='/submit/:shareId'>
-              <SubmitView />
-            </Route>
-          </Switch>
-        </Router>
-      </main>
-      <div className='text-chambray-600 text-center border-chambray-600 text-xs'>
-        Developed by Christoffer A Træen -
-        <a target='_blank' href='https://github.com/freshfish70/secret-share'>
-          {' '}
-          GitHub
-        </a>
+    <QueryClientProvider client={queryClient}>
+      <div className='container mx-auto pb-10'>
+        <h1 className='pt-10 text-lg'>SECRET SHARE</h1>
+        <span className='text-xs uppercase text-chambray-600'>
+          one way share using pub/priv keys
+        </span>
+        <main className='flex justify-center my-10'>
+          <Router>
+            <Switch>
+              <Route exact path='/'>
+                <IndexView />
+              </Route>
+              <Route path='/retrieve/:shareId'>
+                <RetrieveView />
+              </Route>
+              <Route path='/submit/:shareId'>
+                <SubmitView />
+              </Route>
+            </Switch>
+          </Router>
+        </main>
+        <div className='text-chambray-600 text-center border-chambray-600 text-xs'>
+          Developed by Christoffer A Træen -
+          <a target='_blank' href='https://github.com/freshfish70/secret-share'>
+            {' '}
+            GitHub
+          </a>
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   )
 }
 
